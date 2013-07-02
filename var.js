5331c81378caf5596d0a900d8f1eec3c09d46802
joaograca <joao_graca_ei@hotmail.com>
2013-07-02 06:15:07 +0100

    variable type name added

--- var.js
+++ var.js
@@ -1,8 +1,9 @@
-var Var = function(name_,type_,value_,level_) {
+var Var = function(name_,type_,value_,level_,typeName_) {
 	this.name_ = name_;
 	this.type_ = type_;
 	this.value_ = value_;
 	this.level_ = level_;
+	this.typeName_ = typeName_;
 };
 
 module.exports=Var;
