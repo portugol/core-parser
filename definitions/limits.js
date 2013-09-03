16ef1b224d9e25a113faa510f16205e18000c75d
joaograca <joao_graca_ei@hotmail.com>
2013-09-03 22:39:46 +0100

    infinity and NaN checkers

--- definitions/limits.js
+++ definitions/limits.js
@@ -1,9 +1,29 @@
+var types=require('./token_types');
+
 var self ={
-	isInfinite: function(value){
-		if(isFinite(value)){
+	isNotNumber: function(value,type){
+		//Filtrar apenas números porque Strings devolvem true no método isNaN()
+		if(type==types.INTEGER || type==types.REAL){
+			if(isNaN(value)){
+				return true;
+			}
 			return false;
 		}
-		return true;
+		else{
+			return false;
+		}
+	},
+	isInfinite: function(value,type){
+		//Filtrar apenas números porque uma String "infinity" é considerado infinito
+		if(type==types.INTEGER || type==types.REAL){
+			if(isFinite(value)){
+				return false;
+			}
+			return true;
+		}
+		else{
+			return false;
+		}
 	},
 	isScientificNotation: function(value){
 		//converte o valor para string, lowercase e determina a posição da letra "e"
