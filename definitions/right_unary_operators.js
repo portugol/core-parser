168a895d59225c551578a1c5e046f0754450a209
joaograca <joao_graca_ei@hotmail.com>
2013-09-03 22:38:54 +0100

    infinity/NaN check and fixes

--- definitions/right_unary_operators.js
+++ definitions/right_unary_operators.js
@@ -2,7 +2,9 @@ var tokenTypes=require('./token_types'),
 binComp=require('../compatibility/binary_comp'),
 comp=require('../compatibility/unary_right_comp'),
 Token=require('../token'),
-limits=require('./limits');
+limits=require('./limits'),
+conversions=require('./conversions'),
+EvaluatorError=require('../errors/evaluator_error');
 
 var ops={
 	"!": factorial
@@ -12,7 +14,8 @@ var finalType={};
 var self ={
 	calculate: function(token1, operatorToken){
 		if(!(checkCompatibility(token1, operatorToken))){
-			throw "A operação não pode ser efectuada com dados deste tipo";
+			var parameters=[operatorToken.symbol_,"TokenNames."+operatorToken.name_,"VarTypes."+conversions.codeToVarType(token1.type_)];
+			throw new EvaluatorError("INCOMPATIBLE_UNARY_OPERATION",parameters);
 		}
 		//guarda a função javascript que faz a operação (de acordo com o símbolo operatório)
 		var func=ops[operatorToken.value_];
@@ -24,6 +27,16 @@ var self ={
 		var value1 =token1.value_;
 		//guarda o resultado da operação
 		var result =func(value1);
+
+		//verificar se o valor é infinito
+		if(limits.isNotNumber(result,finalType)){
+			throw new EvaluatorError("NOT_A_NUMBER");
+		}
+		//verificar se o valor é infinito
+		if(limits.isInfinite(result,finalType)){
+			throw new EvaluatorError("INFINITY");
+		}
+
 		var symbol;
 		if(finalType==tokenTypes.INTEGER){
 			result=parseInt(result,10);
@@ -52,8 +65,7 @@ function checkCompatibility(token1, operatorToken){
 
 function factorial(value){
 	if(value<0){
-		//console.log(value);
-		throw "Factorial de número negativo";
+		throw new EvaluatorError("NEGATIVE_FACTORIAL",[value]);
 	}
 	if(value===0){
 		return 1;
