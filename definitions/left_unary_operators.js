52a94ca5d3057b8c5c79275d1f1f56b9d35bc772
joaograca <joao_graca_ei@hotmail.com>
2013-09-03 22:37:40 +0100

    infinity/NaN check and fixes

--- definitions/left_unary_operators.js
+++ definitions/left_unary_operators.js
@@ -3,7 +3,9 @@ binComp=require('../compatibility/binary_comp'),
 comp=require('../compatibility/unary_left_comp'),
 Token=require('../token'),
 limits=require('./limits'),
-dictionaryFuncs=require('./dictionary_funcs');
+dictionaryFuncs=require('./dictionary_funcs'),
+conversions=require('./conversions'),
+EvaluatorError=require('../errors/evaluator_error');
 
 var ops={
 	"!": logicNot,
@@ -16,7 +18,8 @@ var finalType={};
 var self ={
 	calculate: function(token1, operatorToken, dictionary){
 		if(!(checkCompatibility(token1, operatorToken))){
-			throw "A operação não pode ser efectuada com dados deste tipo";
+			var parameters=[operatorToken.symbol_,"TokenNames."+operatorToken.name_,"VarTypes."+conversions.codeToVarType(token1.type_)];
+			throw new EvaluatorError("INCOMPATIBLE_UNARY_OPERATION",parameters);
 		}
 		//guarda a função javascript que faz a operação (de acordo com o símbolo operatório)
 		var func=ops[operatorToken.value_];
@@ -28,14 +31,24 @@ var self ={
 		var value1 =token1.value_;
 		//guarda o resultado da operação
 		var result =func(value1);
+
+		//verificar se o valor é infinito
+		if(limits.isNotNumber(result,finalType)){
+			throw new EvaluatorError("NOT_A_NUMBER");
+		}
+		//verificar se o valor é infinito
+		if(limits.isInfinite(result,finalType)){
+			throw new EvaluatorError("INFINITY");
+		}
+
 		var symbol;
 		if(finalType==tokenTypes.INTEGER){
-			result=parseInt(result,10);
+			result=conversions.getIntValue(result,finalType);
 			symbol=result.toString();
 			return new Token(tokenTypes.INTEGER, result, symbol);
 		}
 		if(finalType==tokenTypes.REAL){
-			result=parseFloat(result.toPrecision(12));
+			result=conversions.getRealValue(result,finalType);
 			symbol=result.toString();
 			return new Token(tokenTypes.REAL, result, symbol);
 		}
