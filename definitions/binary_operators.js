e6d95615f2df6572ae8dd2878b8109d961c8a4a4
joaograca <joao_graca_ei@hotmail.com>
2013-09-03 22:36:37 +0100

    infinity/NaN check and fixes

--- definitions/binary_operators.js
+++ definitions/binary_operators.js
@@ -1,7 +1,8 @@
 var tokenTypes=require('./token_types'),
 Token=require('../token'),
 limits=require('./limits'),
-conversions=require('./conversions');
+conversions=require('./conversions'),
+EvaluatorError=require('../errors/evaluator_error');
 
 var ops={
 	"+": add,
@@ -22,7 +23,9 @@ var finalType={};
 var self ={
 	calculate: function(token1, token2, operatorToken){
 		if(!(conversions.checkCompatibility(token1, token2, operatorToken))){
-			throw "Operação entre tipos incompatíveis";
+			console.log(operatorToken);
+			var parameters=[operatorToken.symbol_,"TokenNames."+operatorToken.name_,"VarTypes."+conversions.codeToVarType(token1.type_),"VarTypes."+conversions.codeToVarType(token2.type_)];
+			throw new EvaluatorError("INCOMPATIBLE_BINARY_OPERATION",parameters);
 		}
 		finalType=conversions.getFinalType(token1,token2);
 		//guarda a função javascript que faz a operação (de acordo com o símbolo operatório)
@@ -36,6 +39,16 @@ var self ={
 
 		//guarda o resultado da operação
 		var result =func(value1,value2);
+
+		//verificar se o valor é infinito
+		if(limits.isNotNumber(result,finalType)){
+			throw new EvaluatorError("NOT_A_NUMBER");
+		}
+		//verificar se o valor é infinito
+		if(limits.isInfinite(result,finalType)){
+			throw new EvaluatorError("INFINITY");
+		}
+
 		var symbol;
 		if(finalType==tokenTypes.INTEGER){
 			result=conversions.getIntValue(result,finalType);
@@ -43,7 +56,7 @@ var self ={
 			return new Token(tokenTypes.INTEGER, result, symbol);
 		}
 		if(finalType==tokenTypes.REAL){
-			result=conversions.getRealValue(result.toPrecision(12));
+			result=conversions.getRealValue(result);
 			symbol=result.toString();
 			return new Token(tokenTypes.REAL, result, symbol);
 		}
@@ -71,8 +84,8 @@ function mul(value1,value2){
 }
 
 function div(value1,value2){
-	if(value2===0){
-		throw "divisao por 0";
+	if(value2==0){
+		throw new EvaluatorError("DIVISION_BY_0");
 	}
 	return value1/value2;
 }
@@ -83,30 +96,6 @@ function mod(value1,value2){
 
 function pow(value1,value2){
 	return Math.pow(value1,value2);
-}
-
-function equals(value1,value2){
-	return (value1==value2);
-}
-
-function isMinEqual(value1,value2){
-	return (value1<=value2);
-}
-
-function isMajEqual(value1,value2){
-	return (value1>=value2);
-}
-
-function isMaj(value1,value2){
-	return (value1>value2);
-}
-
-function isMin(value1,value2){
-	return (value1<value2);
-}
-
-function isDif(value1,value2){
-	return (value1!=value2);
 }
 
 function shiftLeft(value1,value2){
@@ -122,18 +111,6 @@ function shiftRight(value1,value2){
 		return value1*(Math.pow(2,value2));
 	}
 	return value1/(Math.pow(2,value2));
-}
-
-function logicNot(value){
-	return (!value);
-}
-
-function logicOr(value1,value2){
-	return (value1 || value2);
-}
-
-function logicAnd(value1,value2){
-	return (value1 && value2);
 }
 
 function bitwiseOr(value1,value2){
